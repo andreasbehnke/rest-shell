8b63c6af88e75bfb492e74c7a95fd77ee1ee45b7
behnkea <andreas.behnke@haufe-lexware.net>
2015-12-11 10:00:57 +0100

    Use quotation marks for variable assignment

--- base.sh
+++ base.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ ! -z "$payload" ] && payload='-d '$payload
+[ ! -z "$payload" ] && payload="-d $payload"
 curl -v -k -H "Content-Type:application/json" -X $method -b cookies.txt -c cookies.txt $payload $url
