272b9b5a500dd113b400b9a06e0592f8a6871243
behnkea <andreas.behnke@haufe-lexware.net>
2015-12-11 10:12:00 +0100

    Introduced configuration file and configuration property for pretty printing json results

--- base.sh
+++ base.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+. "$DIR/config.sh"
+
+pretty_print_command="python -m json.tool"
 [ ! -z "$payload" ] && payload="-d $payload"
-curl -v -k -H "Content-Type:application/json" -X $method -b cookies.txt -c cookies.txt $payload $url
+curl="curl -v -k -H \"Content-Type:application/json\" -X $method -b cookies.txt -c cookies.txt $payload $url"
+
+if [ "$pretty_print" = true ] ; then
+    $curl | $pretty_print_command
+else
+    $curl
+fi
+
