22c6a2272a6f1744ae8d2462a1c03e9b860842bf
behnkea <andreas.behnke@haufe-lexware.net>
2015-12-14 10:32:16 +0100

    Removed quotation marks from content-type header

--- base.sh
+++ base.sh
@@ -3,7 +3,7 @@
 
 pretty_print_command="python -m json.tool"
 [ ! -z "$payload" ] && payload="-d $payload"
-curl="curl -v -k -H \"Content-Type:application/json\" -X $method -b cookies.txt -c cookies.txt $payload $url"
+curl="curl -v -k -H Content-Type:application/json -X $method -b cookies.txt -c cookies.txt $payload $url"
 
 if [ "$pretty_print" = true ] ; then
     $curl | $pretty_print_command
